488cd9bac920fa1de0d1b3182a28258eae36db0f
rschwartz1223 <rschwartz1223@gmail.com>
2019-01-28 19:00:38 -0800

    function implementation

--- Black-Scholes/european_vanila_option.cpp
+++ Black-Scholes/european_vanila_option.cpp
@@ -3,14 +3,37 @@
  Created by Robert Schwartz on 1/28/19.
  Copyright © 2019 Robert Schwartz. All rights reserved. */
 
+#define _USE_MATH_DEFINES
+
 #include "european_vanilla_option.h"
-#include <cmath> //log10(), 
+#include <cmath> //pow(), exp(), modf(), M_PI, log10()
+
+const double golden_ratio = 1.61803398875; //golden ratio approximation
+
+/* cumulative distribution function, calculate the integral using Golden Ratio.
+ @param n amount of samples
+ @param d value of d1 or d2
+ @return average value of all samples */
+double Option::cdf(double n, double d)
+{
+    double x = d / 2; //starting position
+    double intpart = 1.0; //intpart for modf()
+    double sum = 0.0;
+    //sum the sample values
+    for (int i = 0; i < n; ++i)
+        sum += pow(exp(modf((x + golden_ratio) * i, &intpart)), -(pow(d, 2) / 2));
+    return (sum / n) / pow(2 * M_PI, 0.5);
+}
 
 //calculate first parameter of CDF
-double Call::d1(const double S, const double K, const double T, const double sigma, const double r)
+double Option::d1(const double S, const double K, const double T, const double sigma, const double r)
 {
     return (log10(S / K) + (r + (pow(sigma, 2)) / 2) * T) / (sigma * pow(T, 0.5));
 }
 
 //calculate second parameter of CDF
-double Call::d2(const double
+double Option::d2(const double S, const double K, const double T, const double sigma, const double r)
+{
+    double d1 = Option::d1(S, K, T, sigma, r);
+    return d1 - sigma * pow(T, 0.5);
+}
--- Black-Scholes/european_vanilla_option.h
+++ Black-Scholes/european_vanilla_option.h
@@ -9,7 +9,7 @@
 const double r = 0.026; //1 year treasury rate (1/25/19)
 
 //European vanilla call option
-class Call
+class Option
 {
 private:
     double S; //underlying asset price
@@ -18,74 +18,38 @@ private:
     double sigma; //volatility of underlying asset
     
 public:
+    //default constructor
+    Option()
+    {
+        set_S(0.0);
+        set_K(0.0);
+        set_T(0.0);
+        set_sigma(0.0);
+    }
+    //constructor
+    Option(double _S, double _K, double _T, double _sigma)
+    {
+        set_S(_S);
+        set_K(_K);
+        set_T(_T);
+        set_sigma(_sigma);
+    }
+    //setters
     void set_S(double a) { S = a; }
     void set_K(double b) { K = b; }
     void set_T(double c) { T = c; }
     void set_sigma(double d) { sigma = d; }
-    
+    //getters
     const double& get_S() { return S; }
     const double& get_K() { return K; }
     const double& get_T() { return T; }
     const double& get_sigma() { return sigma; }
-    
+    //first parameter for cdf
     double d1(const double S, const double K, const double T, const double sigma, const double r);
-    double d2(const double sigma, const double T);
-    
-    //default constructor
-    Call()
-    {
-        set_S(0.0);
-        set_K(0.0);
-        set_T(0.0);
-        set_sigma(0.0);
-    }
-    //constructor
-    Call(double _S, double _K, double _T, double _sigma)
-    {
-        set_S(_S);
-        set_K(_K);
-        set_T(_T);
-        set_sigma(_sigma);
-    }
-    
-};
-
-//European vanilla put option
-class Put
-{
-private:
-    double S; //underlying asset price
-    double K; //strike price
-    double T; //maturity
-    double sigma; //volatility of underlying asset
-    
-public:
-    void set_S(double a) { S = a; }
-    void set_K(double b) { K = b; }
-    void set_T(double c) { T = c; }
-    void set_sigma(double d) { sigma = d; }
-    
-    const double& get_S() { return S; }
-    const double& get_K() { return K; }
-    const double& get_T() { return T; }
-    const double& get_sigma() { return sigma; }
-    
-    //default constructor
-    Put()
-    {
-        set_S(0.0);
-        set_K(0.0);
-        set_T(0.0);
-        set_sigma(0.0);
-    }
-    //constructor
-    Put(double _S, double _K, double _T, double _sigma)
-    {
-        set_S(_S);
-        set_K(_K);
-        set_T(_T);
-        set_sigma(_sigma);
-    }
+    //second parameter for cdf
+    double d2(const double S, const double K, const double T, const double sigma, const double r);
+    //cumulative distribution function
+    double cdf(double n, double d);
     
 };
 
